a84cca38b34dd4e54087651eec84f70781e7da84
katar1ne <katarineleite5@gmail.com>
2021-06-14 15:45:41 -0300

    Update PensionatoList.java

--- src/aplicacoes/PensionatoList.java
+++ src/aplicacoes/PensionatoList.java
@@ -1,9 +1,10 @@
 package aplicacoes;
 
-import java.util.Scanner;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 import entidades.EntidadesPensionatoList;
 
